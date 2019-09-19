60f01e0a94569ef22236e8b6468b8c44d33e7a79
Moritz Wright <me@moritzwright.com>
2019-09-19 16:15:52 +0200

    favicon

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ require("dotenv").config();
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const express = require("express");
-const favicon = require("serve-favicon");
 const hbs = require("hbs");
 const mongoose = require("mongoose");
 const logger = require("morgan");
@@ -149,7 +148,6 @@ const partialsPath = path.join(__dirname, "./views/partials");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
 app.use(express.static(path.join(__dirname, "public")));
-app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
 
 
 hbs.registerPartials(partialsPath)
